3674c138e44c076b92f3baaeabad0da850a00179
randyfortier <randy.fortier@gmail.com>
2020-02-26 20:27:34 -0500

    Added particle effects demo

---
+++ 06a_BuildingALevel/Assets/Scripts/ExplodingBarrel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingBarrel : MonoBehaviour {
+    [SerializeField] float radius = 3;
+    [SerializeField] GameObject explosionEffect = null;
+    [SerializeField] Transform explosionPoint = null;
+    [SerializeField] int damage = 100;
+    [SerializeField] bool isDestroyed = false;
+
+    private List<ExplodingBarrel> barrelsToExplode = null;
+
+    private void Start() {
+        barrelsToExplode = new List<ExplodingBarrel>();
+    }
+
+    public void Explode() {
+        Instantiate(explosionEffect, explosionPoint.position, Quaternion.identity);
+        Destroy(transform.gameObject, 0.25f);
+
+        // see if any players are nearby to damage
+        LayerMask playerMask = LayerMask.GetMask("Player");
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, playerMask);
+        if (hits.Length > 0) {
+            Health health = hits[0].GetComponent<Health>();
+            health.TakeDamage(damage);
+        }
+
+        // see if any enemies are nearby to damage
+        LayerMask enemyMask = LayerMask.GetMask("Enemies");
+        hits = Physics.OverlapSphere(transform.position, radius, enemyMask);
+        if (hits.Length > 0) {
+            Health health = hits[0].GetComponent<Health>();
+            health.TakeDamage(damage);
+        }
+
+        // prevent an infinite cascade, remember that this barrel is already destroyed
+        isDestroyed = true;
+
+        // see if any other barrels are nearby to cascade
+        LayerMask barrelMask = LayerMask.GetMask("Barrels");
+        hits = Physics.OverlapSphere(transform.position, radius, barrelMask);
+        barrelsToExplode.Clear();
+        for (int i = 0; i < hits.Length; i++) {
+            ExplodingBarrel barrel = hits[i].GetComponent<ExplodingBarrel>();
+            if (!barrel.isDestroyed) {
+                barrelsToExplode.Add(barrel);
+            }
+        }
+        if (barrelsToExplode.Count > 0) {
+            CascadeBarrels();
+        }
+    }
+
+    void CascadeBarrels() {
+        foreach (ExplodingBarrel barrel in barrelsToExplode) {
+            barrel.Explode();
+        }
+    }
+}
---
+++ 06a_BuildingALevel/Assets/Scripts/FirstPersonInput.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class FirstPersonInput : MonoBehaviour {
+    [SerializeField] private Transform camera = null;
+    [SerializeField] private Animator gunAnimator = null;
+    [SerializeField] private GameObject bulletHolePrefab = null;
+    [SerializeField] private GameObject muzzleFlashPrefab = null;
+    [SerializeField] private Transform firePoint = null;
+    [SerializeField] private AudioSource audioSource = null;
+
+    float range = 100f;
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        RaycastHit hit;
+
+        LayerMask enemyMask = LayerMask.GetMask("Enemies");
+        LayerMask barrelMask = LayerMask.GetMask("Barrels");
+        LayerMask groundMask = LayerMask.GetMask("Ground");
+
+        gunAnimator.SetTrigger("Fire");
+        audioSource.PlayDelayed(0.1f);
+        Instantiate(muzzleFlashPrefab, firePoint.position, firePoint.rotation);
+
+        if (Physics.Raycast(camera.position, camera.forward, out hit, range, enemyMask)) {
+            Debug.Log("Shot an enemy thing:" + hit.collider.name);
+
+            Health enemyHealth = hit.collider.GetComponent<Health>();
+            enemyHealth.TakeDamage(10);
+            if (enemyHealth.isDead) {
+                hit.collider.gameObject.SetActive(false);
+            }
+        } else if (Physics.Raycast(camera.position, camera.forward, out hit, range, barrelMask)) {
+            // Make the barrel explode (if it is a barrel)
+            ExplodingBarrel barrel = hit.collider.GetComponent<ExplodingBarrel>();
+            if (barrel != null) {
+                barrel.Explode();
+                Destroy(barrel.gameObject, 0.1f);
+            }
+        } else if (Physics.Raycast(camera.position, camera.forward, out hit, range, groundMask)) {
+            Debug.Log("Shot a wall thing:" + hit.collider.name);
+
+            Instantiate(bulletHolePrefab, 
+                hit.point + (0.01f * hit.normal), 
+                Quaternion.LookRotation(-1 * hit.normal, hit.transform.up));
+        }
+    }
+}
