8f2fd3af6e556341d153373d83cd43bdfdaa233f
randyfortier <randy.fortier@gmail.com>
2020-03-11 18:46:55 -0400

    Added enemy AI demo

---
+++ 06a_BuildingALevel/Assets/EnemyAIStateMachine.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum EnemyState {
+    Patrolling, Alerted, TargetVisible, Dead
+}
+
+public class EnemyAIStateMachine : MonoBehaviour {
+    [SerializeField] private EnemyState currentState = EnemyState.Patrolling;
+
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private int waypointIndex = 0;
+    [SerializeField] private bool patrolLoop = true;
+    [SerializeField] private float closeEnoughDistance;
+
+    [SerializeField] private float lastAlertTime = 0.0f;
+    [SerializeField] private float alertCooldown = 8.0f;
+    [SerializeField] private Vector3 lastKnownTargetPosition;
+
+    [SerializeField] private float lastShootTime = 0.0f;
+    [SerializeField] private float shootCooldown = 1.0f;
+    [SerializeField] private Transform target = null;
+
+    private Animator animator = null;
+    private NavMeshAgent agent = null;
+
+    private void Start() {
+        currentState = EnemyState.Patrolling;
+
+        animator = GetComponent<Animator>();
+        animator.speed = 0.5f;
+
+        agent = GetComponent<NavMeshAgent>();
+
+        if (agent != null && waypoints.Length > 0) {
+            agent.SetDestination(waypoints[waypointIndex].position);
+        }
+    }
+
+    public EnemyState GetState() {
+        return currentState;
+    }
+
+    public void SetState(EnemyState newState) {
+        if (currentState == newState)
+            return;
+
+        currentState = newState;
+
+        if (newState == EnemyState.Patrolling) {
+            // resume patrol
+            agent.enabled = true;
+            waypointIndex = 0;
+        } else if (newState == EnemyState.Alerted) {
+            // investigate the last known position
+            agent.enabled = true;
+            agent.SetDestination(lastKnownTargetPosition);
+
+            // remember when we were alerted
+            lastAlertTime = Time.time;
+        } else if (newState == EnemyState.TargetVisible) {
+            // shoot at the player
+            agent.enabled = false;
+            animator.SetFloat("Speed", 0.0f);
+            lastKnownTargetPosition = target.transform.position;
+        } else if (newState == EnemyState.Dead) {
+            // disable navigation and play death animation
+            agent.enabled = false;
+            animator.SetFloat("Speed", 0.0f);
+            animator.SetBool("Dead", true);
+        }
+    }
+
+    public void SetTarget(Transform target) {
+        this.target = target;
+    }
+
+    private void Update() {
+        if (currentState == EnemyState.Dead) {
+            return;
+        } else if (currentState == EnemyState.Patrolling) {
+            Patrol();
+        } else if (currentState == EnemyState.Alerted) {
+            // check for timeout
+            if (Time.time > (lastAlertTime + alertCooldown)) {
+                SetState(EnemyState.Patrolling);
+                Patrol();
+            } else {
+                Alert();
+            }
+        } else if (currentState == EnemyState.TargetVisible) {
+            Shoot();
+        }
+    }
+
+    private void Patrol() {
+        Vector3 destination = waypoints[waypointIndex].position;
+        float distanceToTarget = Vector3.Distance(agent.transform.position, destination);
+
+        if (distanceToTarget < closeEnoughDistance) {
+            waypointIndex++;
+
+            // loop, if desired
+            if (waypointIndex >= waypoints.Length) {
+                if (patrolLoop) {
+                    waypointIndex = 0;
+                } else {
+                    animator.SetFloat("Speed", 0);
+                    return;
+                }
+            }
+
+            agent.SetDestination(waypoints[waypointIndex].position);
+        }
+
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+
+    private void Alert() {
+        float distanceToTarget = Vector3.Distance(agent.transform.position, lastKnownTargetPosition);
+
+        if (distanceToTarget < closeEnoughDistance) {
+            // stay here
+            animator.SetFloat("Speed", 0.0f);
+
+            // TODO:  Look around animation
+        } else {
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+        }
+    }
+
+    private void Shoot() {
+        if (Time.time > (lastShootTime + shootCooldown)) {
+            Vector3 targetDirection = (target.transform.position - transform.position).normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.15f);
+
+            lastShootTime = Time.time;
+            animator.SetTrigger("Shoot");
+
+            // TODO:  Do raycast to calculate damage
+        }
+
+    }
+}
--- 06a_BuildingALevel/Assets/Scripts/FieldOfView.cs
+++ 06a_BuildingALevel/Assets/Scripts/FieldOfView.cs
@@ -12,6 +12,8 @@ public class FieldOfView : MonoBehaviour {
     [SerializeField] private LayerMask wallLayer;
     [SerializeField] private float targetScanDelay = 0.25f;
 
+    [SerializeField] private EnemyAIStateMachine stateMachine;
+
     public void OnDrawGizmos() {
         Gizmos.matrix = Matrix4x4.identity;
 
@@ -20,6 +22,15 @@ public class FieldOfView : MonoBehaviour {
         Vector3 focusPos = eye.position + eye.forward * visionRadius;
         focusPos.y = eye.position.y;
 
+        if (stateMachine != null) {
+            if (stateMachine.GetState() == EnemyState.TargetVisible) {
+                Gizmos.color = Color.red;
+            } else if (stateMachine.GetState() == EnemyState.Alerted) {
+                Gizmos.color = Color.yellow;
+            }
+        } else {
+            Gizmos.color = Color.green;
+        }
         Gizmos.DrawWireSphere(eye.position, 0.2f);
         Gizmos.DrawWireSphere(eye.position, visionRadius);
         Gizmos.DrawLine(eye.position, focusPos);
@@ -28,6 +39,8 @@ public class FieldOfView : MonoBehaviour {
     private void Start() {
         StartCoroutine("KeepSearchingForTargets", targetScanDelay);
         //StartCoroutine(KeepSearchingForTargets(targetScanDelay)); // equivalent
+
+        stateMachine = GetComponent<EnemyAIStateMachine>();
     }
 
     IEnumerator KeepSearchingForTargets(float delay) {
@@ -39,10 +52,33 @@ public class FieldOfView : MonoBehaviour {
 
     private void LookForTargets() {
         // 1. any objects within our vision radius
-        // 2. is the object within fov?
-        // 3. do we have line of sight?
-        Debug.Log("LookForTargets()");
+        bool canSeeTarget = false;
+        Collider[] targets = Physics.OverlapSphere(eye.position, visionRadius, targetLayer);
+
+        for (int i = 0; i < targets.Length; i++) {
+            Vector3 targetPos = targets[i].transform.position;
+            targetPos.y += 1.0f;
+            Vector3 targetDirection = (targetPos - eye.position).normalized;
+
+            // 2. is the object within fov?
+            if (Vector3.Angle(eye.forward, targetDirection) < fovAngle / 2) {
+                // 3. do we have line of sight?
+                float distance = Vector3.Distance(eye.position, targetPos);
+                if (!Physics.Raycast(eye.position, targetDirection, distance, wallLayer)) {
+                    canSeeTarget = true;
+                    isAlerted = true;
+
+                    // change to target visible state
+                    stateMachine.SetTarget(targets[i].transform);
+                    stateMachine.SetState(EnemyState.TargetVisible);
+                    return;
+                }
+            }
+        }
+
+        if (!canSeeTarget && isAlerted) {
+            stateMachine.SetState(EnemyState.Alerted);
+            isAlerted = false;
+        }
     }
-
-
 }
